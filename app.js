6c13cbed3dfa4b700fc96405725238424c2b3c27
Fernanda Kishimoto <fernandakimiek@gmail.com>
2019-06-09 23:11:21 -0300

    created buttons actions

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ new Vue({
     el: '#app',
     data: {
        playerLife: 100,
-       monsterLife: 100,
-       
+       monsterLife: 0,
+       running: false
     },
     computed: {
-        
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
-        
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(especial) {
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, especial)
+        },
+        hurt(prop, min, max, especial) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        }
     },
     watch: {
-        
+        hasResult(value) {
+            if(value) this.running = false
+        }
     }
 })
