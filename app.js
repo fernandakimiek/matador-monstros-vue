723b4be85cfb269fde3c0caf73d50bd6994d8ebb
Fernanda Kishimoto <fernandakimiek@gmail.com>
2019-06-09 21:41:50 -0300

    Project vue created

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+       playerLife: 100,
+       monsterLife: 100,
+       
+    },
+    computed: {
+        
+    },
+    methods: {
+        
+    },
+    watch: {
+        
+    }
+})
